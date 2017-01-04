6567e26b0091207bc9083f2c0d34faac5efe58c9
kalyan.chittapragada@gmail.com <kalyan.chittapragada@gmail.com>
2017-01-04 03:07:39 -0500

    initial commit

---
+++ src/main/java/labs/springframework/invnetorySystem/Main.java
@@ -0,0 +1,11 @@
+package labs.springframework.invnetorySystem;
+
+/**
+ * Created by e-knca on 1/4/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("My Application");
+    }
+}
